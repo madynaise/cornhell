3ff26e4efc794e85e9804e6f7c6b2a76be4a942e
madynaise <jaar.man488@gmail.com>
2026-02-03 17:36:23 -0600

    changed up the physics and added break button (shift)

--- main.lua
+++ main.lua
@@ -9,6 +9,7 @@ function love.load()
   corn.speed = 20
   corn.xv = 0
   corn.yv = 0
+  corn.stop = false
   
   gun = {}
   gun.x = corn.x + 90
@@ -29,8 +30,16 @@ function love.update(dt)
   local dx = mx - gun.x
   local dy = my - gun.y
   
-  
+  corn.stop = false
   --corn
+  if corn.xv == 0 then
+    corn.xv = 0
+  end
+    
+  if corn.yv == 0 then
+    corn.yv = 0
+  end
+  
   corn.y = corn.y + corn.yv
   corn.x = corn.x + corn.xv
   
@@ -66,27 +75,68 @@ function love.update(dt)
    corn.yv = corn.yv - corn.speed * dt
   end
   
+  if love.keyboard.isDown("lshift") then
+    corn.stop = true
+  end
+  
+  if corn.stop == true then
+    if corn.xv > 0 then
+      corn.xv = corn.xv - 20 *dt
+    end
+    
+    if corn.xv < 0 then
+      corn.xv = corn.xv + 20 *dt
+    end
+    
+    if corn.yv > 0 then
+      corn.yv = corn.yv - 20 *dt
+    end
+    
+    if corn.yv < 0 then
+      corn.yv = corn.yv + 20 *dt
+    end
+    
+    if corn.xv == 0 then
+      corn.xv = 0
+    end
+    
+    if corn.yv == 0 then
+      corn.yv = 0
+    end
+  end
+  
+  
   -- borders
   if corn.x < 0 then
-    corn.xv = 5
+    corn.xv = -corn.xv/2
+    corn.x = 1
   end
   
   if corn.x > 720 then
-    corn.xv = -5
+    corn.xv = -corn.xv/2
+    corn.x = 719
   end
   
   if corn.y < 0 then
-    corn.yv = 5
+    corn.yv = -corn.yv/2
+    corn.y = 1
   end
   
   if corn.y > 520 then
-    corn.yv = -5
+    corn.yv = -corn.yv/2
+    corn.y = 519
   end
   
   --gun
   gun.x = corn.x + 90
   gun.y = corn.y + 30
   gun.r = math.atan2(dy, dx)
+  
+  --end(stuff that has to be at the end)
+  if corn.xv > 50 then
+    corn.xv = 30
+  end
+  
   
 end
 
